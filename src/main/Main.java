af57a4799d40e9b385d371339b9faa64c4769b84
unknown <ismedswandana@google.com>
2023-05-17 09:22:12 +0700

    PBO

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import login.Connector;
+import login.Login_page;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+    public static void main(String[] args) {
+        Login_page log = new Login_page();
+        log.setVisible(true);
+    }
+  
+}
